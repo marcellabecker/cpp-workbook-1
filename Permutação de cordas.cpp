ccda21d1dfa054a752b8a91b721d0013c0099af4
marcellabecker <marcellabecker1@gmail.com>
2021-12-01 00:35:02 -0300

    ajustes

--- Permutação de cordas.cpp
+++ Permutação de cordas.cpp
@@ -1,20 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream> 
 #include <string>
 using namespace std;
  
-void permute(string s , string a)
+ // Função para obter permutações da string escrita
+void permute(string s , string a)  //recursividade
 {
-    if(s.length() == 0)
+    if(s.length() == 0) //length vê o comprimento da string
     {
-        cout<<a<<"  "<<endl;
+        cout<<a<<"  "<<endl;  //se for =0 nao tem permutação
         return;
     }
-    for(int i=0 ; i<s.length() ; i++)
+    for(int i=0 ; i<s.length() ; i++) //pecorre as letras
     {
         char ch = s[i];
-        string l = s.substr(0,i);
-        string r = s.substr(i+1);
-        string m = l + r;
+        string l = s.substr(0,i); //Posição do primeiro caractere a ser copiado
+        string r = s.substr(i+1); //proximos caracteres
+        string m = l + r; 
         permute(m , a+ch);
     }
  
--- programa Autoimpressão.cpp
+++ programa Autoimpressão.cpp
@@ -1,12 +1,22 @@
-#include <iostream>
-#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+void imprime();
 
-int main (int argc, char **argv){
-    std::string code_line;
-    std::ifstream code_file("programa Autoimpressão.cpp");
-
-    while (getline(code_file, code_line))
-        std::cout << code_line << std::endl;
-    code_file.close();
-    return 0;
+int main(void)
+{
+    // printf("codigo imprimindo a si proprio");
+    imprime();
+    return EXIT_SUCCESS;
+}
+void imprime()
+{
+    char programa[2000];
+    FILE * fluxo = fopen("programa Autoimpressão.cpp", "r"); //Abre o arquivo cujo nome é especificado no parâmetro 
+    if ( fluxo == 0)
+    {
+        printf("ERRO! O PROGRAMA NAO PODE SER EXECUTADO.\n");
+    }
+    while (fgets(programa, 2000,  fluxo) != NULL) //Lê do fluxo para a cadeia de caracteres string até a quantidade de caracteres.
+        printf("%s", programa);
+    fclose( fluxo);
 }
